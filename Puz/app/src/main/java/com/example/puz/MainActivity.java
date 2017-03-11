d344b6e03d2e5cdb216e0755a673788d669f2052
Chloe <Chloe.Houlihan.2015@rhul.ac.uk>
2017-03-11 21:32:33 +0000

    xml and java for picture challenge. NOT WORKING

--- Puz/app/src/main/java/com/example/puz/MainActivity.java
+++ Puz/app/src/main/java/com/example/puz/MainActivity.java
@@ -4,7 +4,6 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,5 +18,13 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(this, Riddle.class);
         startActivity(intent);
 
+    }
+
+    /**
+     * Response to clicking Challenge 2 button
+     * */
+    public void sendCoffeeShopMessage(View view) {
+        Intent intent = new Intent(this, FindTheObject.class);
+        startActivity(intent);
     }
 }
