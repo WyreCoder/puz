f02db63facd0adff9afb601cd84388b4ee6efdba
K1502768 <kinga.bojarczuk@kcl.ac.uk>
2017-03-11 14:36:08 +0000

    Initial android project

---
+++ Puz/app/src/main/java/com/example/puz/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.puz;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    /** Called when the user taps Hint 1 button*/
+    public void sendMessage(View view) {
+        Intent intent = new Intent(this, Riddle.class);
+        startActivity(intent);
+
+    }
+}
