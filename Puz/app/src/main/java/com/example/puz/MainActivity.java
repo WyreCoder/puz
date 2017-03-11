d9d738b7b8edc0f8ff463d10cf6d4825bf5981dc
K1502768 <kinga.bojarczuk@kcl.ac.uk>
2017-03-11 17:24:02 +0000

    Added create text riddle view with question and answers provided

--- Puz/app/src/main/java/com/example/puz/MainActivity.java
+++ Puz/app/src/main/java/com/example/puz/MainActivity.java
@@ -6,17 +6,33 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
 
+public class MainActivity extends AppCompatActivity {
+    public static final String EXTRA_MESSAGE = "message";
+    public static final String EXTRA_MESSAGE2 = "message2";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
 
+
     /** Called when the user taps Hint 1 button*/
     public void showRiddle(View view) {
         Intent intent = new Intent(this, Riddle.class);
+        startActivity(intent);
+
+    }
+
+    public void showTextRiddle(View view) {
+        Intent intent = new Intent(this, TextRiddle.class);
+        String message = "Get the question form the server here";
+        ArrayList<String>  answers = new ArrayList();
+        answers.add("choc");
+        answers.add("chocolate");
+        intent.putExtra(EXTRA_MESSAGE, message);
+        intent.putStringArrayListExtra(EXTRA_MESSAGE2, answers);
         startActivity(intent);
 
     }
---
+++ Puz/app/src/main/java/com/example/puz/TextRiddle.java
@@ -0,0 +1,52 @@
+package com.example.puz;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+public class TextRiddle extends AppCompatActivity {
+
+    private ArrayList<String> answers;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_text_riddle);
+
+        // Get the Intent that started this activity and extract the string
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+        answers = intent.getStringArrayListExtra(MainActivity.EXTRA_MESSAGE2);
+
+        // Capture the layout's TextView and set the string as its text
+        TextView textView = (TextView) findViewById(R.id.textView);
+        textView.setText(message);
+    }
+
+    /** Called when the user taps Hint 1 button*/
+    public void checkAnswer(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+        EditText answer = (EditText) findViewById(R.id.editText2);
+        ArrayList<String> a = new ArrayList<>();
+        a.add("lala");
+        if(answers.contains(answer.getText().toString())){
+            Toast toast = Toast.makeText(TextRiddle.this, "GOOD ANSWER", Toast.LENGTH_SHORT);
+            toast.show();
+            startActivity(intent);
+        }
+        else if(answer.getText().toString().length()==0){
+            Toast toast = Toast.makeText(TextRiddle.this, "TYPE AN ANSWER", Toast.LENGTH_SHORT);
+            toast.show();
+        }
+        else{
+            Toast toast = Toast.makeText(TextRiddle.this, "BAD ANSWER", Toast.LENGTH_SHORT);
+            toast.show();
+        }
+    }
+}
